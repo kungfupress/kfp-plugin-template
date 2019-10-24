30b233470c14119c9613921db39c56e84c7de6b5
Juanan Ruiz <juanan@us.es>
2019-10-24 21:23:15 +0200

    Subida inicial

---
+++ kfp-plugin-name.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Plugin Name
+ *
+ * @category Categoría
+ * @package  KFP Plugin Name
+ * @author   Juanan Ruiz <kungfupress@gmail.com>
+ * @license  GPLv2 http://www.gnu.org/licenses/gpl-2.0.txt
+ * @link     https://github.com/kungfupress/plugin-name
+ *
+ * @wordpress-plugin
+ * Plugin Name:  KFP Plugin Name
+ * Plugin URI:   https://github.com/kungfupress/plugin-name
+ * Description:  Descripción del plugin.
+ * Version:      0.0.1
+ * Author:       Juanan Ruiz
+ * Author URI:   https://kungfupress.com/
+ * PHP Version:  5.6
+ */
